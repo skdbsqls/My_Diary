ce9ca4fb2ca12c0f50607b21389a431ad55b994e
skdbsqls <skdbsqls98@naver.com>
2024-02-27 19:10:54 +0900

    react-router-dom 활용

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,9 +1,6 @@
 import { useEffect, useState } from "react";
 
 import Post from "./Post";
-import Modal from "./Modal";
-import NewPost from "./NewPost";
-
 import classes from "./PostsList.module.css";
 
 const PostsList = ({ isPosting, onStopPosting }) => {
@@ -35,11 +32,6 @@ const PostsList = ({ isPosting, onStopPosting }) => {
 
   return (
     <>
-      {isPosting && (
-        <Modal onClose={onStopPosting}>
-          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
-        </Modal>
-      )}
       {!isFetching && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import Posts from "./routes/Posts";
+import NewPost from "./routes/NewPost";
+import RootLayout from "./routes/RootLayout";
 import "./App.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Posts />,
+        children: [{ path: "/create-post", element: <NewPost /> }],
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
