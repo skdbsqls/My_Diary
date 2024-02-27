ad16027948cd1733bacde8ba587310c3d5d9071e
skdbsqls <skdbsqls98@naver.com>
2024-02-27 18:40:52 +0900

    useEffect 활용 및 로딩 상태 관리

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import Post from "./Post";
 import Modal from "./Modal";
@@ -8,6 +8,19 @@ import classes from "./PostsList.module.css";
 
 const PostsList = ({ isPosting, onStopPosting }) => {
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsFetching(true);
+      const respose = await fetch("http://localhost:8080/posts");
+      const resData = await respose.json();
+      setPosts(resData.posts);
+      setIsFetching(false);
+    };
+
+    fetchData();
+  }, []);
 
   const addPostHandler = (postData) => {
     fetch("http://localhost:8080/posts", {
@@ -27,17 +40,22 @@ const PostsList = ({ isPosting, onStopPosting }) => {
           <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      {posts.length > 0 && (
+      {!isFetching && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
             <Post key={post.body} name={post.name} body={post.body} />
           ))}
         </ul>
       )}
-      {posts.length === 0 && (
+      {!isFetching && posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
           <h2>There are no posts yet.</h2>
           <p>Start adding some!</p>
+        </div>
+      )}
+      {isFetching && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <p>Loading posts...</p>
         </div>
       )}
     </>
